9c23ceebb1c54d8c83ef99aa44ca8f4071bf6466
rakhimyakubjanov <rahim1711@gmail.com>
2021-04-12 16:46:57 -0400

    test case reserved

--- testing_commons/testCase1.py
+++ testing_commons/testCase1.py
@@ -4,22 +4,29 @@ import pytest
 import requests
 
 
-@pytest.mark.de
-def test_get():
+
+def test_function():
     url = "https://reqres.in/api/users?page=2"
 
     url_response = requests.get(url)
 
-    response_status_code = url_response.status_code()
+    response_status_code = url_response.status_code
 
     json_text = json.loads(url_response.text)
 
     per_page = jsonpath.jsonpath(json_text, 'per_page')
 
-    students = jsonpath.jsonpath(json_text, 'data')
+    #students = jsonpath.jsonpath(json_text, 'data')
 
+    students = url_response.json()['data']
     assert response_status_code == 200
 
     assert per_page[0] == 6
 
-    assert students == 'data'
+    assert students[0] == {'avatar': 'https://reqres.in/img/faces/7-image.jpg',
+ 'email': 'michael.lawson@reqres.in',
+ 'first_name': 'Michael',
+ 'id': 7,
+ 'last_name': 'Lawson'}
+
+
